b71f82392969419c96ad999f58b4c195ca392807
Khangurai <79690068+Khangurai@users.noreply.github.com>
2023-09-15 20:04:33 +0630

    Add files via upload

---
+++ esp8266.ino
@@ -0,0 +1,31 @@
+/*New blynk app project
+   Home Page
+*/
+
+//Include the library files
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define BLYNK_AUTH_TOKEN "xIuZA8SobD_2aI7vZc5RG2qzF3zvl8sE" //Enter your blynk auth token
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "akk";//Enter your WIFI name
+char pass[] = "akk712001";//Enter your WIFI password
+
+//Get the button value
+BLYNK_WRITE(V0) {
+  digitalWrite(D0, param.asInt());
+}
+
+void setup() {
+  //Set the LED pin as an output pin
+  pinMode(D0, OUTPUT);
+  //Initialize the Blynk library
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+}
+
+void loop() {
+  //Run the Blynk library
+  Blynk.run();
+}
